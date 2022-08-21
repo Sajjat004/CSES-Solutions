1e4149db5722572a882926ff27332066b141d097
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2022-08-21 23:05:22 +0600

    Create Tasks_and_Deadlines.cpp

---
+++ Sorting and Searching/Tasks_and_Deadlines.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long           ll;
+typedef long double         ld;
+typedef unsigned long long  ull;
+typedef pair <int, int>     pii;
+typedef pair <ll, ll>       pll;
+typedef vector <int>        vi;
+typedef vector <ll>         vll;
+
+const int N       = (int) 2e5 + 5;
+const int mxN     = (int) 1e6 + 6;
+const int MOD     = (int) 1e9 + 7;
+const int INF     = (int) 1e9 + 9;
+const double EPS  = (double) 1e-9;
+
+#define debug(x)  cerr << #x << " = " << x << '\n';
+#define all(x)    x.begin(), x.end()
+#define szof(x)   (int) x.size()
+#define ff        first
+#define ss        second
+#define pb        push_back
+#define mp        make_pair
+#define PI        acos(-1.0)
+#define nl        '\n'
+#define Fast_IO   ios_base::sync_with_stdio(false); cin.tie(0);
+
+/*........................ let's try one more time ........................*/
+
+int main() {
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+#endif
+  Fast_IO
+  int test = 1, tc = 0;
+  while (test--) {
+    int n; cin >> n;
+    vector<pii> v(n);
+    for (auto& [x, y] : v) cin >> x >> y;
+    sort(all(v));
+    ll res = 0, cur = 0;
+    for (auto x : v) {
+      cur += x.ff;
+      res += (x.ss - cur);
+    }
+    cout << res << nl;
+  }
+  return 0;
+}
