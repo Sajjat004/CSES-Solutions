7ecfce99b130e612df601c85c8cbbced66e5d90d
Md. Sajjat Hossen <111273800+Sajjat004@users.noreply.github.com>
2025-10-29 11:41:57 +0600

    Implement sliding window minimum algorithm

---
+++ Sliding Window Problems/sliding_window_minimum.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int get(int x, int a, int b, int c) {
+  return (1LL * x * a + b) % c;
+}
+ 
+void run() {
+  int n, k; cin >> n >> k;
+  int x, a, b, c; cin >> x >> a >> b >> c;
+ 
+  int l = x, r = x;
+  deque<int> dq;
+  dq.push_back(x);
+  for (int i = 1; i < k; ++i) {
+    r = get(r, a, b, c);
+    while (!dq.empty() && dq.back() >= r) dq.pop_back();
+    dq.push_back(r);
+  }
+
+  ll jor = dq.front();
+
+  for (int i = k; i < n; ++i) {
+    if (dq.front() == l) dq.pop_front();
+    l = get(l, a, b, c);
+    r = get(r, a, b, c);
+    while (!dq.empty() && dq.back() >= r) dq.pop_back();
+    dq.push_back(r);
+    jor ^= dq.front();
+  }
+ 
+  cout << jor << '\n';
+}
+ 
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  int tests = 1;
+  while (tests--) run();
+ 
+  return 0;
+}
